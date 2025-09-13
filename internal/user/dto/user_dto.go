680264cebf2817ba857a9403158ca35df2e1e9ca
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-09-13 18:12:12 +0700

    refactor: remove password field from UserResponse struct

--- internal/user/dto/user_dto.go
+++ internal/user/dto/user_dto.go
@@ -36,7 +36,6 @@ type UserResponse struct {
 	ID       int    `json:"id"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
-	Password string `json:"password"`
 }
 
 type UserListResponse struct {
